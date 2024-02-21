18a3943a441f18a02b70c248d29523c97c15a582
JieqingShi <JieqingShi@users.noreply.github.com>
2024-02-21 13:44:14 +0100

    Update ci.py

--- ci.py
+++ ci.py
@@ -2,4 +2,4 @@ import wandb
 
 print(f"The version of wand is {wandb.__version__}")
 
-assert wandb.__version__ == "2.1.1", f"Expected version is {2.1.1} but actual version is {wandb.__version__}"
+assert wandb.__version__ == "0.16.3", f"Expected version is 0.16.3 but actual version is {wandb.__version__}"
